a33e204495b4c37f202c513b7f1adc7fa76f7a1f
Pete Jabbour <pete@learnmetrics.com>
2015-01-25 08:33:06 -0600

    changes

--- app/controllers/twitter_accounts_controller.rb
+++ app/controllers/twitter_accounts_controller.rb
@@ -1,6 +1,17 @@
 class TwitterAccountsController < ApplicationController
   
   def index
-    
+    @twitter_accounts = TwitterAccount.order("screen_name ASC").paginate(page: params[:page], per_page: 50)
+    @twitter_account_followings_count = TwitterAccount.where(following: true).where(unfollowed: false).size
+  end
+  
+  def friends
+    @twitter_account_friends = TwitterAccount.where(friends: true).order("screen_name ASC").paginate(page: params[:page], per_page: 50)
+  end
+  
+  def destroy
+    @twitter_account = TwitterAccount.find(params[:id])
+    @twitter_account.destroy
+    redirect_to root_path, notice: "Twitter Account has been removed."
   end
 end
